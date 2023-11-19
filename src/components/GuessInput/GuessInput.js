dac923417c874ff8b6e92b47acb892515bd75b2d
Tanner Hodges <thodges@redventures.com>
2023-11-18 20:35:32 -0500

    Exercise 1: GuessInput

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState('');
+
+  function onSubmit(e) {
+    e.preventDefault();
+    console.log({ guess });
+    setGuess('');
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={onSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        required
+        pattern="[a-zA-Z]{5}"
+        title="5 letters"
+        onChange={(e) => setGuess(e.target.value.toUpperCase())}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
